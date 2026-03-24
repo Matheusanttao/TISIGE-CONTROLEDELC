414c3c8c4b6a76c3b88cd1b65a2024f852c367c7
Matheus Antão <matheusantaosilva18@gmail.com>
2026-03-23 23:01:05 -0300

    ajustes

--- TISIGE2/configLogin.php
+++
@@ -1,9 +0,0 @@
-<?php
-define('HOST','localhost');
-define('USER','root');
-define('PASS','');
-define('BASE','ControleLC');
-
-$conn = new MySQLi(HOST, USER, PASS, BASE);
-
-?>
--- TISIGE2/logout.php
+++
@@ -1,8 +0,0 @@
-<?php
-    session_start();
-    unset($_SESSION["username"]);
-    unset($_SESSION["tipo"]);
-    session_destroy();
-    header("Location: LogIndex.php");
-    exit;
-?>
